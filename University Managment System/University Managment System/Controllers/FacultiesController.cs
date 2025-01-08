4c6af3dd9eb0bda683c833ee89db068667b2d496
MERO434 <meroboini@gmail.com>
2025-01-08 18:27:46 +0200

    sec commit

---
+++ University Managment System/University Managment System/Controllers/FacultiesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace University_Managment_System.Controllers
+{
+    public class FacultiesController : Controller
+    {
+        private readonly UniContext _context;
+
+        public FacultiesController(UniContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Faculties
+        public async Task<IActionResult> Index()
+        {
+            var uniContext = _context.Faculties.Include(f => f.Role);
+            return View(await uniContext.ToListAsync());
+        }
+
+        // GET: Faculties/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties
+                .Include(f => f.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // GET: Faculties/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName");
+            return View();
+        }
+
+        // POST: Faculties/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,PasswordHash,RoleId")] Faculty faculty)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(faculty);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName", faculty.RoleId);
+            return View(faculty);
+        }
+
+        // GET: Faculties/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties.FindAsync(id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName", faculty.RoleId);
+            return View(faculty);
+        }
+
+        // POST: Faculties/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,PasswordHash,RoleId")] Faculty faculty)
+        {
+            if (id != faculty.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(faculty);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacultyExists(faculty.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName", faculty.RoleId);
+            return View(faculty);
+        }
+
+        // GET: Faculties/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var faculty = await _context.Faculties
+                .Include(f => f.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (faculty == null)
+            {
+                return NotFound();
+            }
+
+            return View(faculty);
+        }
+
+        // POST: Faculties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var faculty = await _context.Faculties.FindAsync(id);
+            if (faculty != null)
+            {
+                _context.Faculties.Remove(faculty);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacultyExists(int id)
+        {
+            return _context.Faculties.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ University Managment System/University Managment System/Controllers/TeachingAssistantsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace University_Managment_System.Controllers
+{
+    public class TeachingAssistantsController : Controller
+    {
+        private readonly UniContext _context;
+
+        public TeachingAssistantsController(UniContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TeachingAssistants
+        public async Task<IActionResult> Index()
+        {
+            var uniContext = _context.TeachingAssistants.Include(t => t.Department).Include(t => t.Role);
+            return View(await uniContext.ToListAsync());
+        }
+
+        // GET: TeachingAssistants/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teachingAssistant = await _context.TeachingAssistants
+                .Include(t => t.Department)
+                .Include(t => t.Role)
+                .FirstOrDefaultAsync(m => m.TeachingAssistantId == id);
+            if (teachingAssistant == null)
+            {
+                return NotFound();
+            }
+
+            return View(teachingAssistant);
+        }
+
+        // GET: TeachingAssistants/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name");
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName");
+            return View();
+        }
+
+        // POST: TeachingAssistants/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeachingAssistantId,FirstName,LastName,Email,DepartmentId,PhoneNumber,OfficeHours,RoleDescription,RoleId")] TeachingAssistant teachingAssistant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(teachingAssistant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", teachingAssistant.DepartmentId);
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName", teachingAssistant.RoleId);
+            return View(teachingAssistant);
+        }
+
+        // GET: TeachingAssistants/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teachingAssistant = await _context.TeachingAssistants.FindAsync(id);
+            if (teachingAssistant == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", teachingAssistant.DepartmentId);
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName", teachingAssistant.RoleId);
+            return View(teachingAssistant);
+        }
+
+        // POST: TeachingAssistants/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TeachingAssistantId,FirstName,LastName,Email,DepartmentId,PhoneNumber,OfficeHours,RoleDescription,RoleId")] TeachingAssistant teachingAssistant)
+        {
+            if (id != teachingAssistant.TeachingAssistantId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(teachingAssistant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeachingAssistantExists(teachingAssistant.TeachingAssistantId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", teachingAssistant.DepartmentId);
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "RoleName", teachingAssistant.RoleId);
+            return View(teachingAssistant);
+        }
+
+        // GET: TeachingAssistants/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var teachingAssistant = await _context.TeachingAssistants
+                .Include(t => t.Department)
+                .Include(t => t.Role)
+                .FirstOrDefaultAsync(m => m.TeachingAssistantId == id);
+            if (teachingAssistant == null)
+            {
+                return NotFound();
+            }
+
+            return View(teachingAssistant);
+        }
+
+        // POST: TeachingAssistants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var teachingAssistant = await _context.TeachingAssistants.FindAsync(id);
+            if (teachingAssistant != null)
+            {
+                _context.TeachingAssistants.Remove(teachingAssistant);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeachingAssistantExists(int id)
+        {
+            return _context.TeachingAssistants.Any(e => e.TeachingAssistantId == id);
+        }
+    }
+}
